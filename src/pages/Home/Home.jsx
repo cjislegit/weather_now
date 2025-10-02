d3e8da2b61c29dde02cf79628f8c025afd4ca924
cjislegit <carlosjramirez11@gmail.com>
2025-10-01 21:52:54 -0700

    Created container for nav

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+import NavBar from '../../components/NavBar/NavBar';
+import styles from './Home.module.css';
+
+const Home = () => {
+  return (
+    <div className={styles.homeContainer}>
+      <NavBar />
+    </div>
+  );
+};
+
+export default Home;
