3c825468b421565a5c85eed40af415e7786e71dc
cjislegit <carlosjramirez11@gmail.com>
2025-10-02 19:50:09 -0700

    Set logo and vars for color in the index.css file

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,5 +1,13 @@
+import logo from '../../assets/images/logo.svg';
+
 const NavBar = () => {
-  return <nav></nav>;
+  return (
+    <nav>
+      <div>
+        <img src={logo} alt='Logo' />
+      </div>
+    </nav>
+  );
 };
 
 export default NavBar;
