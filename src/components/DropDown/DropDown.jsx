64586f2cede27b132a0ec6fba028aec19f69eb45
cjislegit <carlosjramirez11@gmail.com>
2025-10-08 17:17:18 -0700

    Added styling to drop down

--- src/components/DropDown/DropDown.jsx
+++ src/components/DropDown/DropDown.jsx
@@ -13,6 +13,21 @@ const DropDown = ({ title }) => {
       <div className={style.dropDownList}>
         <div className={style.dropDownUnits}>
           <div className={style.dropDownUnitsTitle}>Switch to Imperial</div>
+          <div className={style.dropDownUnitsSection}>
+            <div className={style.dropDownUnitsSectionTitle}>Temperature</div>
+            <div className={style.dropDownUnitsOption}>Celsius (°C)</div>
+            <div className={style.dropDownUnitsOption}>Fahrenheit (°F)</div>
+          </div>
+          <div className={style.dropDownUnitsSection}>
+            <div className={style.dropDownUnitsSectionTitle}>Wind Speed</div>
+            <div className={style.dropDownUnitsOption}>km/h</div>
+            <div className={style.dropDownUnitsOption}>mph</div>
+          </div>
+          <div className={style.dropDownUnitsSection}>
+            <div className={style.dropDownUnitsSectionTitle}>Precipitation</div>
+            <div className={style.dropDownUnitsOption}>Millimeters (mm)</div>
+            <div className={style.dropDownUnitsOption}>Inches (In)</div>
+          </div>
         </div>
         <div className={style.dropDownDays}></div>
       </div>
