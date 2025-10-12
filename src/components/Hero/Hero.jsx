e8cae170f763972d7f1ca347ee223d8095b4c15b
cjislegit <carlosjramirez11@gmail.com>
2025-10-11 18:38:43 -0700

    Added the mobile styling for the hero

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,9 @@
+import styles from './Hero.module.css';
+
+const Hero = () => {
+  return (
+    <div className={styles.heroContainer}>How's the sky looking today?</div>
+  );
+};
+
+export default Hero;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,14 @@
+import Hero from '../../components/Hero/Hero';
 import NavBar from '../../components/NavBar/NavBar';
 import styles from './Home.module.css';
 
 const Home = () => {
-  return <div className={styles.homeContainer}>{<NavBar />}</div>;
+  return (
+    <div className={styles.homeContainer}>
+      <NavBar />
+      <Hero />
+    </div>
+  );
 };
 
 export default Home;
