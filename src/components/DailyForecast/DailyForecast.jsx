ce4b1d972aa3fc5de789e5ddc0b5694d2254ae8a
cjislegit <carlosjramirez11@gmail.com>
2025-10-15 16:02:57 -0700

    Styled the forecast cards for mobile

--- src/components/DailyForecast/DailyForecast.jsx
+++ src/components/DailyForecast/DailyForecast.jsx
@@ -1,64 +1,79 @@
 import styles from './DailyForecast.module.css';
+import rain from '../../assets/images/icon-rain.webp';
 
 const DailyForecast = () => {
   return (
     <div className={styles.dailyForecastContainer}>
-      <div className={styles.dailyForecastTitle}>Daily Forecast</div>
+      <div className={styles.dailyForecastTitle}>Daily forecast</div>
       <div className={styles.dailyForecastCardContainer}>
         <div className={styles.dailyForecastCard}>
           <div className={styles.day}>Mon</div>
-          <div className={styles.icon}>☀️</div>
+          <div className={styles.icon}>
+            <img src={rain} alt='' />
+          </div>
           <div className={styles.range}>
-            <div className={styles.low}>75°F</div>
-            <div className={styles.high}>85°F</div>
+            <div className={styles.low}>75°</div>
+            <div className={styles.high}>85°</div>
           </div>
         </div>
         <div className={styles.dailyForecastCard}>
           <div className={styles.day}>Mon</div>
-          <div className={styles.icon}>☀️</div>
+          <div className={styles.icon}>
+            <img src={rain} alt='' />
+          </div>
           <div className={styles.range}>
-            <div className={styles.low}>75°F</div>
-            <div className={styles.high}>85°F</div>
+            <div className={styles.low}>75°</div>
+            <div className={styles.high}>85°</div>
           </div>
         </div>
         <div className={styles.dailyForecastCard}>
           <div className={styles.day}>Mon</div>
-          <div className={styles.icon}>☀️</div>
+          <div className={styles.icon}>
+            <img src={rain} alt='' />
+          </div>
           <div className={styles.range}>
-            <div className={styles.low}>75°F</div>
-            <div className={styles.high}>85°F</div>
+            <div className={styles.low}>75°</div>
+            <div className={styles.high}>85°</div>
           </div>
         </div>
         <div className={styles.dailyForecastCard}>
           <div className={styles.day}>Mon</div>
-          <div className={styles.icon}>☀️</div>
+          <div className={styles.icon}>
+            <img src={rain} alt='' />
+          </div>
           <div className={styles.range}>
-            <div className={styles.low}>75°F</div>
-            <div className={styles.high}>85°F</div>
+            <div className={styles.low}>75°</div>
+            <div className={styles.high}>85°</div>
           </div>
         </div>
         <div className={styles.dailyForecastCard}>
           <div className={styles.day}>Mon</div>
-          <div className={styles.icon}>☀️</div>
+          <div className={styles.icon}>
+            <img src={rain} alt='' />
+          </div>
           <div className={styles.range}>
-            <div className={styles.low}>75°F</div>
-            <div className={styles.high}>85°F</div>
+            <div className={styles.low}>75°</div>
+            <div className={styles.high}>85°</div>
           </div>
         </div>
         <div className={styles.dailyForecastCard}>
           <div className={styles.day}>Mon</div>
-          <div className={styles.icon}>☀️</div>
+          <div className={styles.icon}>
+            <img src={rain} alt='' />
+          </div>
           <div className={styles.range}>
-            <div className={styles.low}>75°F</div>
-            <div className={styles.high}>85°F</div>
+            <div className={styles.low}>75°</div>
+            <div className={styles.high}>85°</div>
           </div>
         </div>
         <div className={styles.dailyForecastCard}>
           <div className={styles.day}>Mon</div>
-          <div className={styles.icon}>☀️</div>
+          <div className={styles.icon}>
+            <img src={rain} alt='' />
+          </div>
           <div className={styles.range}>
-            <div className={styles.low}>75°F</div>
-            <div className={styles.high}>85°F</div>
+            <div className={styles.low}>75°</div>
+            <div className={styles.high}>85°</div>
           </div>
         </div>
       </div>
