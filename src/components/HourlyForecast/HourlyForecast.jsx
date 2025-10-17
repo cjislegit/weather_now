d2f4d17915dad3b75d2f96f0eaf8894b40fac0b0
Carlos J. Ramirez <carlosjramirez11@gmail.com>
2025-10-16 21:11:14 -0700

    Added the dropdown component

--- src/components/HourlyForecast/HourlyForecast.jsx
+++ src/components/HourlyForecast/HourlyForecast.jsx
@@ -1,7 +1,15 @@
 import styles from './HourlyForecast.module.css';
+import DropDown from '../DropDown/DropDown';
 
 const HourlyForecast = () => {
-  return <div className={styles.hourlyForecastContainer}></div>;
+  return (
+    <div className={styles.hourlyForecastContainer}>
+      <div className={styles.hourlyForecastHeader}>
+        <div>Hourly forecast</div>
+        <DropDown />
+      </div>
+    </div>
+  );
 };
 
 export default HourlyForecast;
