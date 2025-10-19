900cfecbea244fa687744660cf8d81b3f282137f
Carlos J. Ramirez <carlosjramirez11@gmail.com>
2025-10-19 14:20:42 -0700

    Added the elements for the hour cards

--- src/components/HourlyForecast/HourlyForecast.jsx
+++ src/components/HourlyForecast/HourlyForecast.jsx
@@ -1,5 +1,6 @@
 import styles from './HourlyForecast.module.css';
 import DropDown from '../DropDown/DropDown';
+import rain from '../../assets/images/icon-rain.webp';
 
 const HourlyForecast = () => {
   return (
@@ -7,6 +8,17 @@ const HourlyForecast = () => {
       <div className={styles.hourlyForecastHeader}>
         <div className={styles.hourlyForecastTitle}>Hourly forecast</div>
         <DropDown />
+      </div>
+      <div className={styles.hourlyForecastCardContainer}>
+        <div className={styles.hourlyForecastCard}>
+          <div className={styles.left}>
+            <img src={rain} alt='rain icon' />
+            <div className={styles.time}>1 PM</div>
+          </div>
+          <div className={styles.right}>
+            <div className={styles.temp}>20</div>
+          </div>
+        </div>
       </div>
     </div>
   );
