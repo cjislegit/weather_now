d0e1f5d07d26c3baabbe67f3d410289a270d2a86
cjislegit <carlosjramirez11@gmail.com>
2025-10-19 19:44:24 -0700

    Set a limit on the navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -5,11 +5,13 @@ import DropDown from '../DropDown/DropDown';
 const NavBar = () => {
   return (
     <nav className={styles.navBarContainer}>
-      <div className={styles.logoContainer}>
-        <img className={styles.logo} src={logo} alt='Logo' />
-      </div>
-      <div className={styles.unitsContainer}>
-        <DropDown type='units' />
+      <div className={styles.navBarLimit}>
+        <div className={styles.logoContainer}>
+          <img className={styles.logo} src={logo} alt='Logo' />
+        </div>
+        <div className={styles.unitsContainer}>
+          <DropDown type='units' />
+        </div>
       </div>
     </nav>
   );
