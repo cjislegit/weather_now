c1d1d818e1dc8d4f325d88bd8f156d67cc7febed
cjislegit <carlosjramirez11@gmail.com>
2025-10-23 19:45:30 -0700

    Home page is getting location of user

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,3 +1,5 @@
+import { useEffect, useState } from 'react';
+
 import Hero from '../../components/Hero/Hero';
 import NavBar from '../../components/NavBar/NavBar';
 import SearchBar from '../../components/SearchBar/SearchBar';
@@ -5,6 +7,32 @@ import WeatherLayout from '../../layout/Weather/WeatherLayout';
 import styles from './Home.module.css';
 
 const Home = () => {
+  const [location, setLocation] = useState(null);
+  const [error, setError] = useState(null);
+
+  const getLocation = () => {
+    if (!navigator.geolocation) {
+      setError('Geolocation is not supported by your browser');
+      return;
+    }
+
+    navigator.geolocation.getCurrentPosition(
+      (pos) => {
+        const { latitude, longitude } = pos.coords;
+        setLocation({ latitude, longitude });
+      },
+      (err) => {
+        setError(err.message);
+      }
+    );
+  };
+
+  useEffect(() => {
+    getLocation();
+  }, []);
+
+  console.log('User location:', location);
+
   return (
     <div className={styles.homeContainer}>
       <NavBar />
