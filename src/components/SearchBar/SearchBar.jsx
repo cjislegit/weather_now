0231c14f047f130043dab8fd9932cfb8707291ef
cjislegit <carlosjramirez11@gmail.com>
2025-11-29 16:08:01 -0800

    Value of search is now tied to state

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,12 +1,12 @@
 import styles from './SearchBar.module.css';
 import search from '../../assets/images/icon-search.svg';
 
-const SearchBar = () => {
+const SearchBar = ({ cities, setCities }) => {
   return (
     <div className={styles.searchBarContainer}>
       <div className={styles.search}>
         <img src={search} alt='search' />
-        <input type='text' placeholder='Search for a place...' />
+        <input type='text' placeholder='Search for a place...' onChange={(e) => setCities(e.target.value)} value={cities} />
       </div>
       <input type='button' value='Search' />
     </div>
