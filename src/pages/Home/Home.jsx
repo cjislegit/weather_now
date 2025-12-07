d6d032ce0cf22ba7243bfeaa893d236bffca6ff5
Carlos J. Ramirez <carlosjramirez11@gmail.com>
2025-12-07 15:43:14 -0800

    The location name will use a max of 50% of the container on nonmobile screens

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -14,7 +14,6 @@ const Home = () => {
   const [city, setCity] = useState('Fullerton, California, United States');
   const [todayDate, setTodayDate] = useState('');
   const [error, setError] = useState(null);
-  const [cities, setCities] = useState([]);
 
   useEffect(() => {
     const now = new Date();
